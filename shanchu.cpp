f4ac6bf2f7c0353524ae852413639b91fad5a127
WinfengWon <winfengwon@gmail.com>
2024-01-02 20:56:49 +0800

    Init

---
+++ shanchu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"sss!!"<<flush;
+    int a=5;
+    int b=322;
+    int c;
+    c=a+b;
+    printf("\n%d\n",c);
+    return 0;
+}
